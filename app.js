07b5a987a7b0f4d553d4a327cf2a68e9e9d3874b
hudaaddelson@gmail.com <hudaaddelson@gmail.com>
2022-06-06 20:35:09 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function tampilJam() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 23;
+    }
+
+    if (h > 23) {
+        h = h - 23;
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s;
+    document.getElementById("jamDigital").textContent = time;
+
+    setTimeout(tampilJam, 1000);
+}
+
+tampilJam();
